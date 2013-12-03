cf7b96777cabb15128139ee745f5cb17802a2380
Laya Salarbehzadi <layasb@yahoo.com>
2013-12-02 23:06:44 -0800

    commit for final roject

--- homework7/homework7/Controllers/HomeController.cs
+++ homework7/homework7/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace homework7.Controllers
+namespace CourseProject.Controllers
 {
     public class HomeController : Controller
     {
@@ -19,9 +19,85 @@ namespace homework7.Controllers
         {
             return View();
         }
+        public ActionResult AbsoluteLayout()
+        {
+            return View();
+        }
+        public ActionResult StaticLayout()
+        {
+            return View();
+        }
+        public ActionResult FloatLayout()
+        {
+            return View();
+        }
+        public ActionResult Task3()
+        {
+            return View();
+        }
+        public ActionResult FormatPhoneNumber(string inputNumber)
+        {
+
+
+            if (string.IsNullOrEmpty(inputNumber))
+            {
+                return Content("invalid input, please try again");
+            }
+            else
+            {
+                string formatted = formatNumber(inputNumber);
+                return View((object)formatted);
+            }
+        }
+
+
+        // please implement your phone number formatting logic here
+        private string formatNumber(string inputNumber)
+        {
+            // imiplement formatting here
+            string sub1 = inputNumber.Substring(0, 3);
+            string sub2 = inputNumber.Substring(3, 3);
+            string sub3 = inputNumber.Substring(6, 4);
+            return "(" + sub1 + ")" + " " + sub2 + "-" + sub3;
+
+        }
+
+
+
+        public ActionResult Task4()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Task4(string inputString)
+        {
+            if (string.IsNullOrEmpty(inputString))
+            {
+                //shouldn't be possible anymore
+                return Content("invalid input, please try again");
+            }
+            else
+            {
+                string FinalString = stringReversal(inputString);
+                return View("textFormat", (object)FinalString);
+            }
+        }
+        private string stringReversal(string input)
+        {
+            string[] words = input.Split(' ');
+            string words2 = "";
+            for (int i = words.Length; i > 0; i -= 1)
+            {
+                words2 += (words[i - 1] + " ");
+            }
+            return words2;
+        }
         public ActionResult AboutMe()
         {
             return View();
         }
+
     }
 }
